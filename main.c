70ea7cfc8eb451033d7871267e032f575761469c
Eetu <99178022+E3TU@users.noreply.github.com>
2025-03-06 20:49:24 +0200

    Rock paper scissors logic

--- main.c
+++ main.c
@@ -1,7 +1,70 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main(void)
 {
-    printf("Hello, World!\n");
+    char user_choice;
+    int random;
+
+    srand(time(NULL));
+    random = rand()%3;
+
+    printf("Welcome to rock paper scissors!\n");
+    printf("Enter your choice R for Rock, P for Paper, S for Scissors: ");
+    scanf("%c", &user_choice);
+
+
+    printf("You entered: %c\n", user_choice);
+    if (random == 0 && user_choice == 'R') {
+        printf("Computer chose Rock\n");
+        printf("Tie!\n");
+    }
+
+    if (random == 1 && user_choice == 'R') {
+        printf("Computer chose Paper\n");
+        printf("You lost!\n");
+    }
+
+    if (random == 2 && user_choice == 'R') {
+        printf("Computer chose Scissors\n");
+        printf("You won!\n");
+    }
+
+    if (random == 0 && user_choice == 'P') {
+        printf("Computer chose Rock\n");
+        printf("You won!\n");
+    }
+
+    if (random == 1 && user_choice == 'P') {
+        printf("Computer chose Paper\n");
+        printf("Tie!\n");
+    }
+
+    if (random == 2 && user_choice == 'P') {
+        printf("Computer chose Scissors\n");
+        printf("You lost!\n");
+    }
+
+    if (random == 0 && user_choice == 'S') {
+        printf("Computer chose Rock\n");
+        printf("You lost!\n");
+    }
+
+    if (random == 1 && user_choice == 'S') {
+        printf("Computer chose Paper\n");
+        printf("You won!\n");
+    }
+
+    if (random == 2 && user_choice == 'S') {
+        printf("Computer chose Scissors\n");
+        printf("Tie!\n");
+    }
+
+    // if (user_choice != 'R' || user_choice != 'P' || user_choice !='S') {
+    //     printf("You entered an invalid choice\n");
+    // } else {
+    //
+    // }
     return 0;
 }
